90cd61c0b42ef3483d4f67038515367b85feaa45
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-04-05 13:20:04 +0200

    modification index.js

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/index.scss';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import './styles/index.css';
+import Home from './pages/Home';
+import Apropos from './pages/Apropos';
+import Logement from './pages/Logement';
+import Error from './pages/Error';
+import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/aPropos" component={Apropos}/>
+        <Route path="/logement/:id" component={Logement}/>
+        <Route component={Error} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
