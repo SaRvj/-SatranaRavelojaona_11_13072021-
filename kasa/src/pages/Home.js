048e82a7a73c43050cda380e6742fa61d72ef868
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-04-05 15:10:46 +0200

    modification de const en class

--- kasa/src/pages/Home.js
+++ kasa/src/pages/Home.js
@@ -1,48 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Header from '../components/Header';
-import Background from '../assets/BannerHome.png';
-import Card from '../components/Card';
-import Footer from '../components/Footer';
-import LoadingPage from './LoadingPage';
+import React from 'react';
+import '../styles/sass/pages/Home.scss';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Banner from '../components/Banner'
+import ListLogements from '../components/ListLogements'
+import img from './../assets/bannerHomepage.png'
 
-const Home = () => {
-  const [isLoading, setLoader] = useState(true)
-  const [logementsList, setLogementsList] = useState([])
-  useEffect(() => {
-    getData()
-    setLoader(false)
-  },[])
-  const getData = () => {
-    axios
-    .get('logements.json')
-    .then((res) => setLogementsList(res.data))
-    .catch(err => console.log(err))
-  }
-  if(!isLoading){
-    return (
-      <div className="home">
-        <Header />
-        <div className="home-content">
-          <div className="intro">
-            <img id="background" src={Background} alt="Kasa" />
-            <p>Chez vous, partout et ailleurs</p>
-          </div>
-          
-          <div className="gallery">
-            {logementsList.map((logement) => (
-              <Card logement={logement} key={logement.id}/>
-            ))}
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }else{
+class Home extends React.Component {
+  render(){
     return(
-      <div><LoadingPage /></div>
+      <div className="Home">
+      <Header />
+      <Banner background={img} title="Chez vous, partout et ailleurs"/>
+      <ListLogements />
+      <Footer />
+    </div>
     )
-  }  
-};
+  }
+}
 
 export default Home;
