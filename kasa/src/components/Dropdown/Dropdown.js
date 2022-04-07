6a57f0ac9481ca57bfe21bd4245f7b4671c6abab
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-04-07 09:01:44 +0200

    ajout composant pour les dropdown

---
+++ kasa/src/components/Dropdown/Dropdown.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./dropdown.css";
+import arrow from "../../assets/arrow.svg";
+
+//Ce composant est une Dropdown factory, utilisée dans la page À propos et la page Hébergement
+export default class Dropdown extends Component {
+  showDropdown(e) {
+    e.target.closest("div").children[1].classList.toggle("dropdown_arrow_hidden");
+    e.target.closest("div").children[2].classList.toggle("dropdown_hidden");
+  }
+
+  render() {
+    return (
+      <div className="dropdown_container" onClick={this.showDropdown}>
+        <h2 className="dropdown_title">{this.props.title}</h2>
+        <object className="dropdown_arrow dropdown_arrow_hidden" type="image/svg+xml" data={arrow} alt="dropdown_arrow">
+          {"Your browser does not support SVG "}
+        </object>
+        <p className={this.props.className + " dropdown_hidden"}>{this.props.description}</p>
+      </div>
+    );
+  }
+}
