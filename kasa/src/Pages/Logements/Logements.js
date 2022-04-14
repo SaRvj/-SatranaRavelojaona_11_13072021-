22d9abb2ed2b6cc30993c1e5f6bb72cc38d1320c
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-04-14 20:55:21 +0200

    modifs axios logements

--- kasa/src/Pages/Logements/Logements.js
+++ kasa/src/Pages/Logements/Logements.js
@@ -8,6 +8,8 @@ import { withRouter } from 'react-router'
 import Carousel from '../../components/Carrousel/Carrousel'
 
 class Accomodations extends Component {
+  url = process.env.PUBLIC_URL + '/logements.json';
+
   constructor(props) {
     super(props)
 
@@ -16,20 +18,33 @@ class Accomodations extends Component {
     }
   }
 
-  getAccomodation(id) {
-    const accomodation = data.find((item) => id === item.id)
-    if (!accomodation) {
-      this.props.history.push('/error404')
-      return null
+  /**
+   * Récupère les données.
+   * Voir syntaxe async functions ici: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function.
+   * 
+   * @param {any} id 
+   * @return {object | null}
+   */
+  async getAccomodation(id) {
+    try {
+      const response = await axios.get(`${this.url}`);
+      const accomodation = response.data.find((item) => id === item.id);
+      if (!accomodation) {
+        this.props.history.push('/error404')
+        return null
+      }
+      return accomodation
+    } catch (error) {
+      console.error(`Error: ${error}`)
     }
-    return accomodation
   }
 
-  componentDidMount() {
-    const searchParams = new URLSearchParams(this.props.location.search)
+  async componentDidMount() {
+    const searchParams = new URLSearchParams(this.props.location.search);
+    const accomodation = await this.getAccomodation(searchParams.get('id'));
     this.setState({
-      accomodation: this.getAccomodation(searchParams.get('id')),
-    })
+      accomodation,
+    });
   }
 
   render() {
