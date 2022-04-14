e280310445b7d0ed7f89f679dc8399af39cb7011
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-04-14 15:53:47 +0200

    modif import data avec axios

--- kasa/src/Pages/Home/Home.js
+++ kasa/src/Pages/Home/Home.js
@@ -2,21 +2,37 @@ import Banner from '../../components/Banner/Banner.js'
 import Header from '../../components/Header/Header.js'
 import Gallery from '../../components/Gallery/Gallery.js'
 import Footer from '../../components/Footer/Footer.js'
-import data from '../../Data/data.json'
+import axios from 'axios';
+// import data from '../../Data/data.json'
 import bannerUrl from '../../assets/bannerHomepage.png'
 import { Component } from 'react'
 import './home.css'
 
 //Ce composant est la vue de la page Home
 export default class Home extends Component {
+
+  url = process.env.PUBLIC_URL + '/logements.json';
   constructor(props) {
     super(props)
     this.state = {
       bannerUrl: bannerUrl,
       text: 'Chez vous, partout et ailleurs',
       className: 'home',
-      data: data,
+      data: [],
     }
+  }
+
+  componentDidMount() {
+    this.getAllLogements();
+  }
+
+   getAllLogements = () => {
+    axios.get(`${this.url}`)
+    .then ((response) => {
+        //console.log("response", response.data);
+        this.setState({ data: response.data });
+    })
+    .catch(error => console.error(`Error: ${error}`));
   }
 
   render() {
